707d63bbd200fc015c17eb2b4c92e01bf1ab4d9f
wangbaolong <wangbaolong@iseetech.com.cn>
2018-03-07 10:38:09 +0800

    发布版本-01

--- TVWallAX/TVWallAXidl.h
+++ TVWallAX/TVWallAXidl.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Wed Mar 07 10:21:53 2018
+/* at Wed Mar 07 14:14:14 2018
  */
 /* Compiler settings for TVWallAX.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
